ba8d8d5ea575d9f01fba494af8af708703186c23
Mathias Erik Nord <mathieno@stud.ntnu.no>
2025-09-23 22:12:15 +0200

    feat: :lipstick: Added styling to main UI.
    Added theme styling of the app through main.

--- foodtracker/lib/main.dart
+++ foodtracker/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foodtracker/widgets/bottomNav.dart';
 
-/// Flutter code sample for [BottomNavigationBar].
-
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -11,9 +9,55 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "TEST",
-      theme: ThemeData(primarySwatch: Colors.blue),
+      title: "FoodTracker",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.amber, // main brand color
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true, // enables rounded Material 3 widgets
+
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.amber[700],
+          foregroundColor: Colors.white,
+          centerTitle: true,
+          elevation: 0,
+        ),
+
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          selectedItemColor: Colors.amber[700],
+          unselectedItemColor: Colors.grey[600],
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.white,
+          elevation: 8,
+        ),
+
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.amber[50],
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(24),
+            borderSide: BorderSide.none,
+          ),
+          hintStyle: TextStyle(color: Colors.grey[600]),
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 16,
+          ),
+        ),
+
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.amber[700],
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(24),
+            ),
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+          ),
+        ),
+      ),
       home: const CustomBottomNav(),
-      );
+    );
   }
 }
